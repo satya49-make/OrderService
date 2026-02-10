8c0133cd0f618d31e763ef7c64e44ec4692ab531
Satya Yadav <satya@example.com>
2026-02-11 00:54:30 +0530

    junit test cases fix2
    Signed-off-by: Satya Yadav <satya@example.com>

--- src/test/java/com/order/OrderServiceApplicationTests.java
+++ src/test/java/com/order/OrderServiceApplicationTests.java
@@ -2,8 +2,16 @@ package com.order;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"spring.sql.init.mode=never",
+		"spring.jpa.defer-datasource-initialization=false",
+		"spring.datasource.url=jdbc:h2:mem:testdb",
+		"spring.datasource.driverClassName=org.h2.Driver",
+		"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+})
 class OrderServiceApplicationTests {
 
 	@Test
